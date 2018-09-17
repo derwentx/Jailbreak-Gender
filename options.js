e108037333b4bff2325fb2645250188cc0371f90
derwentx <derwentx@gmail.com>
2018-09-17 10:57:39 +1000

    options logic less prone to errors

--- options.js
+++ options.js
@@ -1,7 +1,10 @@
 var startingOptions = JSON.parse(localStorage.getItem("options"));
 
 document.getElementById("checkDaily").onclick = setOptions;
-document.getElementById("checkDaily").checked = startingOptions.checkDaily || false;
+document.getElementById("checkDaily").checked = false;
+if (startingOptions !== null && startingOptions.hasOwnProperty('checkDaily') ) {
+    document.getElementById("checkDaily").checked = startingOptions.checkDaily;
+}
 function setOptions() {
     var options = {
         checkDaily: document.getElementById("checkDaily").checked
